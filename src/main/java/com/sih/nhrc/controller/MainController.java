5cbfd14ba3429cbb59074620fc4b1e3d9ed71ff3
ritik10 <vermark43@gmail.com>
2018-03-30 02:39:19 +0530

    controller change

--- src/main/java/com/sih/nhrc/controller/MainController.java
+++ src/main/java/com/sih/nhrc/controller/MainController.java
@@ -13,7 +13,7 @@ public class MainController {
 	@RequestMapping("/hello")
 	public ModelAndView showMessage(
 			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-		System.out.println("in controller");
+		System.out.println("Hii in controller");
  
 		ModelAndView mv = new ModelAndView("helloworld");
 		mv.addObject("message", message);
